971bf94d049fce48e6d89ea1d8a0f72be072559e
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-18 11:12:43 +0530

    Update ColorCode_prv.h

--- ColorCode_prv.h
+++ ColorCode_prv.h
@@ -1,1 +1,6 @@
-// No content yet
+
+extern int GetPairNumberFromColor(const ColorPair* colorPair);
+
+extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+
+extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
