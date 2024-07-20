2cf39b47bd72b1f701d821f79c1c5e8a815bcabf
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-20 13:59:06 +0530

    Update ColorCode_prv.h

--- ColorCode_prv.h
+++ ColorCode_prv.h
@@ -1,33 +1,15 @@
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+#ifndef COLORCODE_PRV_H
+#define COLORCODE_PRV_H
 
-const char* MajorColorNames[] = 
-{
-    "White", "Red", "Black", "Yellow", "Violet"
-};
+extern const char* MajorColorNames[];
+extern int numberOfMajorColors;
+extern const char* MinorColorNames[];
+extern const int MAX_COLORPAIR_NAME_CHARS;
+extern int numberOfMinorColors;
 
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-const char* MinorColorNames[] = 
-{
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-typedef struct 
-{
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
-extern int GetPairNumberFromColor(const ColorPair* colorPair);
-extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 extern void printColorReferenceManual();
+
+#endif // COLORCODE_PRV_H
