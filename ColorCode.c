dbadb937246cb572618bf1df78dc3a617ac15f01
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-20 13:46:31 +0530

    Update ColorCode.c

--- ColorCode.c
+++ ColorCode.c
@@ -1,17 +1,20 @@
 #include "ColorCode_prv.h"
 
-int GetPairNumberFromColor(const ColorPair* colorPair) {
+int GetPairNumberFromColor(const ColorPair* colorPair) 
+{
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+void ColorPairToString(const ColorPair* colorPair, char* buffer) 
+{
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
 }
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
+ColorPair GetColorFromPairNumber(int pairNumber) 
+{
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = 
