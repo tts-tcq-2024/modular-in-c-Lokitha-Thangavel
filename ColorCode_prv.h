5104874c6ae1614903b49794aa1cca38a621c5a3
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-20 14:05:30 +0530

    Update ColorCode_prv.h

--- ColorCode_prv.h
+++ ColorCode_prv.h
@@ -17,7 +17,8 @@ extern const int MAX_COLORPAIR_NAME_CHARS;
 extern int numberOfMinorColors;
 
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
-extern ColorPair GetColorFromPairNumber(int pairNumber) 
+extern ColorPair GetColorFromPairNumber(int pairNumber);
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer) ;
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 extern void printColorReferenceManual();
