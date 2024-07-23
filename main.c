bd7849ea182366b8f33692ed2195d29e1d859b77
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-23 09:53:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
 
-    printColorReferenceManual();
+    generateColorReferenceManual();
 
     return 0;
 }
